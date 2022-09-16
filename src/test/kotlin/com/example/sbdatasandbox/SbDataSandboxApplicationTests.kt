e8e0f0062a7650a91b99d1205dcdf3a3e0e9dda2
Taito Suzuki <suzuito3@gmail.com>
2022-09-17 07:25:05 +0900

    first commit

---
+++ src/test/kotlin/com/example/sbdatasandbox/SbDataSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sbdatasandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbDataSandboxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
